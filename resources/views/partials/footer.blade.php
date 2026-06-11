01df8eef16c2e68775d303a2407dcde60ad12b1c
liyon joy <liyonjoy08@gmail.com>
2026-06-11 17:21:18 -0400

    refg

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -15,7 +15,7 @@
             <h2>Quick Links</h2>
             <ul>
                 <li><a href="{{ route('home') }}">Home</a></li>
-                <li><a href="{{ route('pages.show', ['section' => 'about', 'page' => 'about-us']) }}">About</a></li>
+                <li><a href="{{ route('pages.show', ['section' => 'about', 'page' => 'us']) }}">About</a></li>
                 <li><a href="{{ route('contact') }}">Contact</a></li>
                 <li><a href="{{ route('pages.show', ['section' => 'products', 'page' => 'healthline']) }}">thehealthline.ca</a></li>
             </ul>
